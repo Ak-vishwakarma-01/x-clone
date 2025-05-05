7c4ba32f4accc1a2f93f3bf4b5191ab25d312f32
Ankit kumar Vishwakarma <100838272+Ak-vishwakarma-01@users.noreply.github.com>
2025-05-05 23:51:46 +0530

    adding something

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,12 @@ import "./globals.css";
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
+}: Readonly<{                //Readonly<{ children: React.ReactNode }> ensures the props are immutable (a common pattern for layout components)
+  children: React.ReactNode; //React.ReactNode is the TypeScript type for anything that can be rendered in React (strings, JSX, numbers, fragments, etc.)
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>{children} test</body>
     </html>
   );
 }
